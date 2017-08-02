e6860996d20427d0a8a4a90d9bbc705ac74f254a
Tio <tiorezafk@yahoo.com>
2017-08-02 19:41:50 +0700

    change method callback

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/tweet/:query/:page', tweet.index);
 router.post('/tweet', tweet.simpanTweet);
-router.post('/tweet/callback', tweet.callbackAuth);
+router.get('/tweet/callback', tweet.callbackAuth);
 
 module.exports = router;
