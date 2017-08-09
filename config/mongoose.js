2b2e0d6c1f4ec4305869e51e1d296dd4278dbd88
Tioreza Febrian <tiorezafk@outlook.com>
2017-08-09 16:07:04 +0000

    mongoose.js edited

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 module.exports = function() {
-	mongoose.connect('mongodb://localhost:27017/divernity');
-	// mongoose.connect('mongodb://divernityapp:divernity2017@ds129333.mlab.com:29333/divernity');
+	//mongoose.connect('mongodb://localhost:27017/divernity');
+	mongoose.connect('mongodb://divernityapp:divernity2017@ds129333.mlab.com:29333/divernity');
 	var db = mongoose.connection
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.on('open',function(){
